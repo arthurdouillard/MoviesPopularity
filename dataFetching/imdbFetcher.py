9ad6982baedbf5d85960c66230ba00e22b5c1f31
Arthur Douillard <douill_a@epita.fr>
2017-07-04 20:23:27 +0200

    fetch: Add options to fetcher.

--- dataFetching/imdbFetcher.py
+++ dataFetching/imdbFetcher.py
@@ -9,52 +9,60 @@ from kafka import KafkaProducer
 import requests
 
 
-def fetch_page(url):
+def fetch_page(url, args):
     while True:
         try:
             response = requests.get(url)
             return response
         except KeyboardInterrupt:
-            print('Exiting program after keyboard interrupt.')
+            if args.verbose:
+                print('Exiting program after keyboard interrupt.')
             sys.exit(1)
         except:
-            print('No internet...\r', end='')
+            if args.verbose:
+                print('No internet...\r', end='')
 
 
-def parse_movies_list():
+def parse_movies_list(args):
     url = 'http://www.the-numbers.com/movie/budgets/all'
-    html = fetch_page(url).text
+    html = fetch_page(url, args).text
     soup = BeautifulSoup(html.encode('utf-8'), 'html.parser')
-    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    producer = KafkaProducer(bootstrap_servers=args.kafka)
     parse_dollars = lambda x: int(x.replace('$', '').replace(',', ''))
 
     movies = soup.find_all('td')
     movies_data = []
-    number_of_movies = 1000
     spaces = ' ' * 80
-    for c, i in enumerate(range(0, 6*number_of_movies, 6)):
+    for c, i in enumerate(range(0, 6 * args.max, 6)):
         name = movies[i+2].text.strip()
-        print('{}\r'.format(spaces), end='')
-        percent = c / number_of_movies * 100
-        print('{} %\t{}\r'.format(percent, name), end='')
+
+        if args.verbose:
+            print('{}\r'.format(spaces), end='')
+            percent = c / args.max * 100
+            print('{} %\t{}\r'.format(percent, name), end='')
 
         movie_data = {}
 
         movie_data['budget'] = parse_dollars(movies[i+3].text)
         movie_data['gross'] = parse_dollars(movies[i+5].text)
-        movie_data.update(get_imdb_data(name))
-        producer.send('movies', json.dumps(movie_data).encode())
+        movie_data.update(get_imdb_data(name, args))
         movies_data.append(movie_data)
 
-    return movies_data
+        producer.send(args.topic, json.dumps(movie_data).encode())
+
+        if args.save is not None and c % args.step == 0:
+            save_data(args.save, movies_data)
+
+    if args.save is not None:
+        save_data(args.save, movies_data)
 
 
-def get_imdb_data(name):
+def get_imdb_data(name, args):
     url_imdb = 'http://www.imdb.com'
 
     # Research with name
     url = '{}/find?ref_=nv_sr_fn&q={}&s=tt'.format(url_imdb, name)
-    response = fetch_page(url)
+    response = fetch_page(url, args)
     if response.status_code != 200:
         return {}
 
@@ -64,7 +72,7 @@ def get_imdb_data(name):
         movie_url = soup.find('td', 'result_text').a['href']
     except:
         return {} # No movie found on imdb.
-    response = fetch_page('{}{}'.format(url_imdb, movie_url))
+    response = fetch_page('{}{}'.format(url_imdb, movie_url), args)
     if response.status_code != 200:
         return {}
     movie = parse_movie(BeautifulSoup(response.text.encode('utf-8'),
@@ -72,7 +80,7 @@ def get_imdb_data(name):
 
     reviews_url = '{}/title/{}/reviews?ref_=tt_urv'\
                     .format(url_imdb, movie_url.split('/')[2])
-    response = fetch_page(reviews_url)
+    response = fetch_page(reviews_url, args)
     if response.status_code != 200:
         movie.update({'reviews': []})
         return movie
@@ -147,9 +155,19 @@ def save_data(path, data):
         f.write(json.dumps(data, indent=2))
 
 
+def file2kafka(args):
+    producer = KafkaProducer(bootstrap_servers=args.kafka)
+    with open(args.load, 'r') as f:
+        json_data = json.load(f)
+
+        for obj in json_data:
+            producer.send(args.topic, json.dumps(json_data[obj]).encode())
+
+
 def logic(argv):
     parser = argparse.ArgumentParser(description="Fetcher")
     parser.add_argument('--kafka', action='store', dest='kafka', required=True)
+    parser.add_argument('--topic', action='store', dest='topic', required=True)
     parser.add_argument('--max', action='store', dest='max', type=int,
                         default=5000)
     parser.add_argument('--verbose', action='store_true', dest='verbose')
@@ -160,10 +178,10 @@ def logic(argv):
 
     args = parser.parse_args(argv)
 
+    if args.load is not None:
+        file2kafka(args)
 
-    movies = parse_movies_list()
-    save_data('imdb.json', movies)
-
+    parse_movies_list(args)
 
 if __name__ == '__main__':
     logic(sys.argv[1:])
