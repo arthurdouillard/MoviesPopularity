0317ce9df93f07b130bdc3405287a23f472dad3a
Arthur Douillard <douill_a@epita.fr>
2017-07-08 17:06:16 +0200

    misc: Parrallelize the sentiment analysis.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,15 @@
 package main.scala
 
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, SparkFiles}
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import kafka.serializer.StringDecoder
-import model.Movie
+import model.{Movie, Review}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.dstream.InputDStream
 import play.api.libs.json.Json
 
 
-/**
-  * @author douill_a
-  * @date 27/06/2017
-  */
 object Main {
 
   implicit val movieFormat = Json.format[Movie]
@@ -23,24 +21,51 @@ object Main {
       System.exit(1)
     }
 
-    val Array(brokers, topics, hdfsPath) = args
 
-    val sc = new SparkConf().setAppName("MoviesPopularity").setMaster("local[4]")
+    val Array(brokers, topic, hdfsPath) = args
+    val sentTopic = "sentiment"
+
+    val sc = new SparkConf().setAppName("MoviesPopularity").setMaster("local[*]")
     val ssc = new StreamingContext(sc, Seconds(2))
 
-    val topicsSet = topics.split(",").toSet
-    val kafkaParams = Map[String, String]("bootstrap.servers" -> brokers)
-    val stream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-      ssc, kafkaParams, topicsSet)
+    val streamRaw = setUpStream(brokers, topic, ssc)
 
-    stream.map(_._2)
-          .map(Json.parse(_).as[Movie])
-          .map(x => (x, calculateFinalScore(x)))
-          .saveAsTextFiles(hdfsPath, "txt")
+
+    ssc.sparkContext.addFile("sentimentAnalysis/sentimentAnalyser.py")
+
+    streamRaw.map(_._2)
+      .foreachRDD(rdd => analyseSentiment(sentTopic, brokers, rdd))
+
+    val streamSent = setUpStream(brokers, sentTopic, ssc)
+    streamSent.map(_._2)
+      .map(Json.parse(_).as[Movie])
+      .map(movie => (movie, calculateFinalScore(movie)))
+      .saveAsTextFiles(hdfsPath, "txt")
 
     ssc.start()
     ssc.awaitTermination()
   }
+
+  def analyseSentiment(topic: String, brokers: String, rdd: RDD[String]) = {
+    val cmdTopic = " --topic " + topic
+    val cmdBrokers = " --brokers " + brokers
+    val cmdClf = " --clf " + "sentimentAnalysis/classifier.pkl"
+    rdd.pipe("python3 " + SparkFiles.get("sentimentAnalyser.py") + cmdTopic + cmdBrokers + cmdClf)
+  }
+
+  def setUpStream(brokers: String, topic: String, ssc: StreamingContext): InputDStream[(String, String)] = {
+
+
+
+    val kafkaParams = Map[String, String]("bootstrap.servers" -> brokers)
+
+    KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+        ssc,
+        kafkaParams,
+        Set(topic)
+    )
+  }
+
 
   def calculateFinalScore(movie: Movie): Float = {
     var total = 0
